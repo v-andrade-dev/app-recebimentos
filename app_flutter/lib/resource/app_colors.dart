6f642a71c79ec9939d9a59d4b81fb401cc529183
v-andrade-dev <vini.andrade0@gmail.com>
2024-09-21 22:40:20 -0300

    Criação da classe base para cores

---
+++ app_flutter/lib/resource/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromARGB(255, 149, 235, 178);
+  static const secondary = Color.fromRGBO(60, 160, 76, 1);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
