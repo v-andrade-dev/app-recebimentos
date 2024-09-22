757eb60500839879b6072edf5caabd64082a01b6
v-andrade-dev <vini.andrade0@gmail.com>
2024-09-21 22:41:25 -0300

    Alterações nas cores da tela de login

--- app_flutter/lib/screens/login.dart
+++ app_flutter/lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:app_flutter/resource/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -12,8 +13,7 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      decoration:
-          const BoxDecoration(color: Color.fromARGB(255, 146, 247, 155)),
+      decoration: const BoxDecoration(color: AppColors.primary),
       child: Center(
         child: Padding(
             padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
@@ -27,19 +27,34 @@ class _LoginState extends State<Login> {
                 SizedBox(
                   width: 400,
                   child: TextFormField(
-                    decoration: const InputDecoration(labelText: "Email"),
+                    decoration: const InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(color: AppColors.black),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: AppColors.black))),
                   ),
                 ),
                 SizedBox(
                   width: 400,
                   child: TextFormField(
-                    decoration: const InputDecoration(labelText: "Senha"),
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(color: AppColors.black),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: AppColors.black))),
                   ),
                 ),
-                SizedBox(
+                Container(
+                  padding: const EdgeInsets.only(top: 8),
                   child: ElevatedButton(
                     onPressed: () {},
-                    child: const Text("Entrar"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.secondary),
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(color: AppColors.white),
+                    ),
                   ),
                 )
               ],
