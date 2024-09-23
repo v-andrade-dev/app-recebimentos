0217a8c417e2bcd7e74e321621a3c137dd005c3c
v-andrade-dev <vini.andrade0@gmail.com>
2024-09-23 18:13:42 -0300

    Criação da navigation bar

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:app_flutter/screens/home.dart';
 import 'package:app_flutter/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
       home: Scaffold(
-    body: Login(),
+    body: Home(),
   )));
 }
--- app_flutter/lib/screens/home.dart
+++ app_flutter/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:app_flutter/resource/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,8 +9,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [
+    Text("Home"),
+    Text("Notificações"),
+    Text("Opções")
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: null,
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.primary,
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notificações"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Opções")
+        ],
+        selectedItemColor: AppColors.secondary,
+      ),
+    );
   }
 }
