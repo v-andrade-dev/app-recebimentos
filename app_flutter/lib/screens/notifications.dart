eed914cd0c2d77d8b8e22a94e3aee85fd4bf0620
v-andrade-dev <vini.andrade0@gmail.com>
2024-09-30 18:29:42 -0300

    Criação da tela de notificações

--- app_flutter/lib/screens/notifications.dart
+++ app_flutter/lib/screens/notifications.dart
@@ -1,10 +1,53 @@
+import 'package:app_flutter/resource/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class Notifications extends StatelessWidget {
+class Notifications extends StatefulWidget {
   const Notifications({super.key});
 
   @override
+  State<Notifications> createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  List<String> list = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  @override
   Widget build(BuildContext context) {
-    return const Text("Notificações");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Notificações",
+        ),
+        backgroundColor: AppColors.secondary,
+      ),
+      body: Container(
+        //decoration: BoxDecoration(color: AppColors.primary.withOpacity(0.3)),
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: ((context, index) {
+            return ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+              child: Card(
+                  //color: AppColors.primary.withOpacity(0.3),
+                  child: Padding(
+                padding: const EdgeInsets.all(4),
+                child: SizedBox(
+                  height: 50,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Titulo"),
+                        Text(
+                          list[index].toString(),
+                        ),
+                      ]),
+                ),
+              )),
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
