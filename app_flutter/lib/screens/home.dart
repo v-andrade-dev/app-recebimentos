2ae8d3ab1d9806851de09f199212bc096601fd81
v-andrade-dev <vini.andrade0@gmail.com>
2024-10-03 17:59:14 -0300

    Alteração na estrutura dos arquivos

--- app_flutter/lib/screens/home.dart
+++ app_flutter/lib/screens/home.dart
@@ -1,8 +1,7 @@
-import 'package:app_flutter/resource/app_colors.dart';
 import 'package:app_flutter/resource/custom_navbar.dart';
 import 'package:app_flutter/screens/notifications.dart';
 import 'package:app_flutter/screens/options.dart';
-import 'package:app_flutter/screens/packages_services/admin_services.dart';
+import 'package:app_flutter/screens/admin_services/admin_services.dart';
 import 'package:app_flutter/screens/packages_services/owner_services.dart';
 import 'package:flutter/material.dart';
 
@@ -54,33 +53,3 @@ class _HomeState extends State<Home> {
     return list;
   }
 }
-
-
-/*
-
-Padding(
-          padding:
-              const EdgeInsets.only(left: 16, right: 16, bottom: 24, top: 0),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(24)),
-            child: BottomNavigationBar(
-              backgroundColor: AppColors.primary,
-              currentIndex: _currentIndex,
-              onTap: (value) {
-                setState(() {
-                  _currentIndex = value;
-                });
-              },
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.notifications), label: "Notificações"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person), label: "Opções")
-              ],
-              selectedItemColor: AppColors.black,
-            ),
-          ),
-        )
-
-*/
