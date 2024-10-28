f79dc720c4eefe1f8d62f93b4759f2d6085d9bbe
v-andrade-dev <vini.andrade0@gmail.com>
2024-10-28 17:23:23 -0300

    Classe http overrides para evitar erro de certificado, ambiente dev, adicionado pacote http

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:app_flutter/screens/home.dart';
 import 'package:app_flutter/screens/login.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  //development only
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MaterialApp(
       home: Scaffold(
     body: Home(),
